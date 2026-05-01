403136e79b64f302766e3e2f91814a2e7159c484
SWE-RkJunaid <rkjunaid2002@gmail.com>
2026-05-01 09:50:22 +0600

    Updatehello.py

--- Practice/hello.py
+++ Practice/hello.py
@@ -3,6 +3,10 @@ print("I am Reduanul karim Junaid")
 num1 = 10 
 num2 = 20
 sum = num1 + num2
+sub = num2 - num1
+div = num2 / num1
 multiple = num1 * num2 
 print(sum )
 print(multiple)
+print(sub)
+print(div)
