cd382e857c846f5f57ae6346af57f8e3a9483cc4
SWE-RkJunaid <rkjunaid2002@gmail.com>
2026-05-01 09:55:27 +0600

    Update hello.py

--- Practice/hello.py
+++ Practice/hello.py
@@ -5,8 +5,11 @@ num2 = 20
 sum = num1 + num2
 sub = num2 - num1
 div = num2 / num1
+reminder = num2 % num1
 multiple = num1 * num2 
 print(sum )
 print(multiple)
 print(sub)
 print(div)
+print(reminder)
+
